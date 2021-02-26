d320ac7e84eada9673ddb38d37fed65c2b37d327
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-02-26 11:00:31 -0500

    add new role

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const { viewTable, viewRoles, viewEmployees } = require('./lib/queries');
+
+// queries
+const { 
+    viewTable, 
+    viewRoles, 
+    viewEmployees,
+    addDepartment,
+    addRole,
+    addEmployee,
+    getDeptId } = require('./lib/queries');
+
+const { get } = require('http');
 
 // create the connection to database
 const connection = mysql.createConnection({
@@ -14,74 +25,167 @@ const connection = mysql.createConnection({
 connection.connect(err => {
     if (err) throw err;
     console.log('connected as id ' + connection.threadId);
+    console.log(`
+    ╔═════════════════════╗
+    ║                     ║
+    ║  Employee Database  ║
+    ║                     ║
+    ╚═════════════════════╝
+    `);
     promptOptions();
 });
 
-console.log(`
-╔═════════════════════╗
-║                     ║
-║  Employee Database  ║
-║                     ║
-╚═════════════════════╝
-`);
 
 const promptOptions = () => {
     return inquirer
     .prompt([{
         type: 'list',
-        name: 'chooseTable',
+        name: 'chooseOption',
         message: 'What would you like to do?',
-        choices: ['View Departments', 'View Roles', 'View Employees', 'Exit']
+        choices: ['View Departments', 'View Roles', 'View Employees', 'Update Departments', 'Update Roles', 'Exit']
     }])
-    .then(({ chooseTable }) => {
-        if (chooseTable === 'View Departments') {
+    .then(({ chooseOption }) => {
+        if (chooseOption === 'View Departments') {
             console.log(`
 ╔═══════════════╗
 ║  Departments  ║
 ╚═══════════════╝
 `);
-            connection.query(viewTable('departments'),
-                function (err, results) {
-                    if (err) throw err;
-                    console.table(results);
-                    return promptOptions();
-                });
-            // connection.end();
+            connectDb(viewTable('departments'), [], true);
         }
-        else if (chooseTable === 'View Roles') {
+        else if (chooseOption === 'View Roles') {
             console.log(`
 ╔═════════╗
 ║  Roles  ║
 ╚═════════╝
 `);
-            connection.query(
-                viewRoles(),
-                function (err, results) {
-                    if (err) throw err;
-                    console.table(results);
-                    return promptOptions();
-                });
-                // connection.end();
+            connectDb(viewRoles(), [], true);
         }
-        else if (chooseTable === 'View Employees') {
+        else if (chooseOption === 'View Employees') {
             console.log(`
 ╔═════════════╗
 ║  Employees  ║
 ╚═════════════╝
 `);
-            connection.query(
-                //viewTable('employees')
-                viewEmployees(),
-                function (err, results) {
-                    if (err) throw err;
-                    console.table(results);
-                    return promptOptions();
-                });
-                // connection.end();
+            connectDb(viewEmployees(), [], true);
+        }
+        else if (chooseOption === 'Update Departments') {
+            promptDeptOptions();
+        }
+        else if (chooseOption === 'Update Roles') {
+            promptRoleOptions();
         }
         else {
-            console.log('✶ Exiting employee database ✶');
+            console.log('\n✶ Exiting employee database ✶\n');
             connection.end();
         }
     });
+};
+
+// connect to database and make query
+const connectDb = (queryFn, params, showTable, message) => {
+    connection.query(
+        queryFn,
+        params,
+        function (err, results) {
+            if (err) throw err;
+            if (showTable) {
+                console.table(results);
+            }
+            else {
+                console.log(message);
+            }
+            return promptOptions();
+        });
+};
+
+// update roles
+const promptRoleOptions = () => {
+    connection.query(
+        viewTable('departments'),
+        function (err, results) {
+            if (err) throw err;
+            // get array of departments
+            const deptOptions = results.map(({ name }) => name);
+            //get array of department ids
+            const deptIds = results.map(({ id }) => id);
+            inquirer.prompt([{
+                type: 'list',
+                name: 'chooseUpdateTask',
+                message: 'What would you like to do?',
+                choices: ['Add Role', 'Remove Role', 'Go Back']
+            }])
+                .then(({ chooseUpdateTask }) => {
+                    switch (chooseUpdateTask) {
+                        case 'Add Role':
+                            return inquirer
+                                .prompt([{
+                                    type: 'list',
+                                    name: 'dept',
+                                    message: 'Select a department',
+                                    choices: deptOptions
+                                },
+                                {
+                                    type: 'text',
+                                    name: 'roleInput',
+                                    message: 'Please enter the name of the new role.'
+                                },
+                                {
+                                    type: 'text',
+                                    name: 'salaryInput',
+                                    message: 'Please enter a salary'
+                                }])
+                                .then(({ dept, roleInput, salaryInput }) => {
+                                    // get corresponding dept id
+                                    const deptIdIndex = deptOptions.indexOf(dept);
+                                    const deptId = deptIds[deptIdIndex];
+                                    
+                                    // insert new role
+                                    connectDb(addRole(), 
+                                    [roleInput, parseFloat(salaryInput), deptId], 
+                                    false, 
+                                    `\mSuccessfully added role ✶ ${roleInput} ✶\n`);
+                                });
+                        case 'Remove Department':
+                            console.log('removing department ...');
+                            promptOptions();
+                            break;
+                        case 'Go Back':
+                            promptOptions();
+                            break;
+                    }
+                });
+        });
+};
+
+// update departments
+const promptDeptOptions = () => {
+    return inquirer
+        .prompt([{
+            type: 'list',
+            name: 'chooseUpdateTask',
+            message: 'What would you like to do?',
+            choices: ['Add Department', 'Remove Department', 'Go Back']
+        }])
+        .then(({ chooseUpdateTask }) => {
+            switch (chooseUpdateTask) {
+                case 'Add Department':
+                    return inquirer
+                    .prompt([{
+                        type: 'text',
+                        name: 'deptInput',
+                        message: 'Please enter the name of the new department.'
+                    }])
+                    .then(({ deptInput }) => {
+                        connectDb(addDepartment(), deptInput, false, `\nSuccessfully added department ✶ ${deptInput} ✶\n`);
+                    });
+                case 'Remove Department':
+                    console.log('removing department ...');
+                    promptOptions();
+                    break;
+                case 'Go Back':
+                    promptOptions();
+                    break;
+            };
+        });
 };
